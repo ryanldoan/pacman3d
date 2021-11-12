cc5694d4a59681b6a54920fdee4a18fb4a232bed
human-bootleg <caojfox@gmail.com>
2021-11-11 19:52:57 -0800

    merged maze and pacman

--- demo3.js
+++ demo3.js
@@ -3,6 +3,7 @@ import {defs, tiny} from './examples/common.js';
 const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
 } = tiny;
+const {Cube, Axis_Arrows, Textured_Phong, Phong_Shader, Basic_Shader, Subdivision_Sphere} = defs
 
 export class Demo3 extends Scene {
     constructor() {
@@ -11,13 +12,16 @@ export class Demo3 extends Scene {
 
         // At the beginning of our program, load one of each of these shape definitions onto the GPU.
         this.shapes = {
+            cube: new Cube(),
             pacman: new defs.Subdivision_Sphere(4),
         };
 
         // *** Materials
         this.materials = {
+            wall: new Material(new defs.Phong_Shader(),
+                {ambient: 0.4, diffusivity: 0.6, color: hex_color("#888888")}),
             pacman: new Material(new defs.Phong_Shader(),
-                {ambient: 1, diffusivity: 0.4, color: hex_color("#FFFF00")}),
+                {ambient: 0.4, diffusivity: 0.6, color: hex_color("#FFFF00")}),
         }
 
         this.initial_camera_location = Mat4.look_at(vec3(0, 5, 5), vec3(0, 0, 0), vec3(0, 1, 0));
@@ -57,6 +61,52 @@ export class Demo3 extends Scene {
         });
     }
 
+    make_walls(context, program_state, left=true){
+        let mirror = 1;
+        if (left)
+            mirror = -1;
+
+        let model_trans_wall_1 = Mat4.translation(-5, 2 - 0.1, 0).times(Mat4.scale(0.33, 1, 10)); 
+        let model_trans_wall_2 = Mat4.translation(+5, 2 - 0.1, 0).times(Mat4.scale(0.33, 1, 10));
+        let model_trans_wall_3 = Mat4.translation(0, 2 - 0.1, -15).times(Mat4.scale(6.25, 1, 0.33));
+        let model_trans_wall_4 = Mat4.translation(+9, 2 - 0.1, 0).times(Mat4.scale(4.25, 1, 0.33)); 
+        let model_trans_wall_5 = Mat4.translation(+15, 2 - 0.1, -10).times(Mat4.scale(0.33, 1, 6.25));
+        let model_trans_wall_6 = Mat4.translation(-11, 2 - 0.1, -10).times(Mat4.scale(6.25, 1, 0.33));
+        let model_trans_wall_7 = Mat4.translation(0, 2 - 0.1, -20).times(Mat4.scale(6.25, 1, 0.33));
+        let model_trans_wall_8 = Mat4.translation(+6, 2 - 0.1, -26).times(Mat4.scale(0.33, 1, 6.25));
+        let model_trans_wall_9 = Mat4.translation(-6, 2 - 0.1, -26).times(Mat4.scale(0.33, 1, 6.25));
+        let model_trans_wall_10 = Mat4.translation(-17, 2 - 0.1, 0).times(Mat4.scale(0.33, 1, 10));
+        let model_trans_wall_11 = Mat4.translation(-11, 2 - 0.1, +10).times(Mat4.scale(6.25, 1, 0.33));
+        let model_trans_wall_12 = Mat4.translation(+15, 2 - 0.1, +10).times(Mat4.scale(0.33, 1, 6.25)); // far right
+        let model_trans_wall_13 = Mat4.translation(-24, 2 - 0.1, -20).times(Mat4.scale(6.25, 1, 0.33));
+        let model_trans_wall_14 = Mat4.translation(-18, 2 - 0.1, -26).times(Mat4.scale(0.33, 1, 6.25));
+        let model_trans_wall_15 = Mat4.translation(-30, 2 - 0.1, -26).times(Mat4.scale(0.33, 1, 6.25));
+        let model_trans_wall_16 = Mat4.translation(-30, 2 - 0.1, -15).times(Mat4.scale(10, 1, 0.33));
+        let model_trans_wall_17 = Mat4.translation(-30, 2 - 0.1, -11).times(Mat4.scale(0.33, 1, 4.25));
+        let model_trans_wall_18 = Mat4.translation(18, 2 - 0.1, -32).times(Mat4.scale(12, 1, 0.33));
+        let model_trans_wall_19 = Mat4.translation(-42, 2 - 0.1, -32).times(Mat4.scale(12, 1, 0.33));
+
+        this.shapes.cube.draw(context, program_state, model_trans_wall_1, this.materials.wall);
+        this.shapes.cube.draw(context, program_state, model_trans_wall_2, this.materials.wall);
+        this.shapes.cube.draw(context, program_state, model_trans_wall_3, this.materials.wall);
+        this.shapes.cube.draw(context, program_state, model_trans_wall_4, this.materials.wall); 
+        this.shapes.cube.draw(context, program_state, model_trans_wall_5, this.materials.wall); 
+        this.shapes.cube.draw(context, program_state, model_trans_wall_6, this.materials.wall); 
+        this.shapes.cube.draw(context, program_state, model_trans_wall_7, this.materials.wall); 
+        this.shapes.cube.draw(context, program_state, model_trans_wall_8, this.materials.wall); 
+        this.shapes.cube.draw(context, program_state, model_trans_wall_9, this.materials.wall); 
+        this.shapes.cube.draw(context, program_state, model_trans_wall_10, this.materials.wall); 
+        this.shapes.cube.draw(context, program_state, model_trans_wall_11, this.materials.wall); 
+        this.shapes.cube.draw(context, program_state, model_trans_wall_12, this.materials.wall); 
+        this.shapes.cube.draw(context, program_state, model_trans_wall_13, this.materials.wall); 
+        this.shapes.cube.draw(context, program_state, model_trans_wall_14, this.materials.wall); 
+        this.shapes.cube.draw(context, program_state, model_trans_wall_15, this.materials.wall); 
+        this.shapes.cube.draw(context, program_state, model_trans_wall_16, this.materials.wall); 
+        this.shapes.cube.draw(context, program_state, model_trans_wall_17, this.materials.wall); 
+        this.shapes.cube.draw(context, program_state, model_trans_wall_18, this.materials.wall); 
+        this.shapes.cube.draw(context, program_state, model_trans_wall_19, this.materials.wall); 
+    }
+
     display(context, program_state) {
         // display():  Called once per frame of animation.
         
@@ -72,10 +122,12 @@ export class Demo3 extends Scene {
 
         const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
 
-        const light_position = vec4(0, 100, 100, 1);
+        const light_position = vec4(0, 10, 10, 1);
 
         // The parameters of the Light are: position, color, size
         program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
+
+        this.make_walls(context, program_state);
         
         let model_transform = Mat4.identity().times(Mat4.scale(1,1,1)).times(Mat4.translation(this.posX, 0, this.posZ));
 
@@ -94,7 +146,7 @@ export class Demo3 extends Scene {
 
         this.shapes.pacman.draw(context, program_state, model_transform, this.materials.pacman);
         if (this.FORWARD != null && this.BACKWARD != null && this.RIGHT != null && this.LEFT != null){
-            let desired = Mat4.inverse(model_transform.times(Mat4.translation(0,0,-1)));
+            let desired = Mat4.inverse(model_transform.times(Mat4.translation(0,0,5)));
             program_state.set_camera(desired);
         }
     }
