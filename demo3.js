8f404d3a02adbd03eae4f85cea1562c356bcff76
human-bootleg <caojfox@gmail.com>
2021-11-12 06:34:22 -0800

    Implemented camera movement with Pacman and option of stationary bird's eye view, intuitive directional controls automatically altered to POV

--- demo3.js
+++ demo3.js
@@ -24,10 +24,13 @@ export class Demo3 extends Scene {
                 {ambient: 0.4, diffusivity: 0.6, color: hex_color("#FFFF00")}),
         }
 
-        this.initial_camera_location = Mat4.look_at(vec3(0, 5, 5), vec3(0, 0, 0), vec3(0, 1, 0));
+        this.model_transform = Mat4.identity();
+        this.initial_camera_location = Mat4.inverse((this.model_transform).times(Mat4.translation(0,3,5)).times(Mat4.rotation(-Math.PI/12,1,0,0)));
+        this.follow = true;
+        this.abs_dir = 'f';
+        this.speed = 1;
         this.posX = 0;
         this.posZ = 0;
-        this.speed = 0;
     }
 
     make_control_panel() {
@@ -51,6 +54,136 @@ export class Demo3 extends Scene {
         this.key_triggered_button("Stop", ["m"], () => {
             this.dir = 's';
         });
+        this.new_line(); this.new_line();
+        this.key_triggered_button("Camera POV", ["c"], () => {
+            let T;
+            if (this.follow){
+                this.model_transform = T = (this.model_transform).times(Mat4.inverse(Mat4.rotation(this.getAngle(this.abs_dir),0,1,0)));
+            }
+            else{
+                T = this.model_transform;
+                this.model_transform = (this.model_transform).times(T).times(Mat4.rotation(this.getAngle(this.abs_dir),0,1,0)).times(Mat4.inverse(T));
+            }
+            this.follow ^= 1;
+        });
+    }
+
+    display(context, program_state) {
+        // display():  Called once per frame of animation.
+        
+        // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
+        if (!context.scratchpad.controls) {
+            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
+            // Define the global camera and projection matrices, which are stored in program_state.
+            program_state.set_camera(this.initial_camera_location);
+        }
+
+        program_state.projection_transform = Mat4.perspective(
+            Math.PI / 4, context.width / context.height, .1, 1000);
+
+        const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
+
+        const light_position = vec4(0, 50, -15, 1);
+
+        // The parameters of the Light are: position, color, size
+        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 5000)];
+
+        this.make_walls(context, program_state);
+
+        let T;
+        const move = -0.1-(this.speed-1)*0.01;
+        
+        if (this.follow){
+            if (this.dir != null)
+                this.model_transform = (this.model_transform).times(Mat4.inverse(Mat4.rotation(this.getAngle(this.abs_dir),0,1,0)));
+            T = this.model_transform;
+            this.abs_dir = this.getNewDir();
+            if (this.dir != null){
+                if (this.dir === 's') this.dir = null;
+                else this.dir = 'f';
+
+                this.model_transform = (this.model_transform).times(Mat4.rotation(this.getAngle(this.abs_dir),0,1,0)).times(Mat4.inverse(T));
+                if (this.dir === 'f')
+                    this.model_transform = (this.model_transform).times(Mat4.translation(0,0,move));
+            }
+        }else{
+            this.abs_dir = this.dir;
+            switch(this.abs_dir){
+                case 'l': T = Mat4.translation(move,0,0);
+                    break;
+                case 'r': T = Mat4.translation(-move,0,0);
+                    break;
+                case 'f': T = Mat4.translation(0,0,move);
+                    break;
+                case 'b': T = Mat4.translation(0,0,-move);
+                    break;
+                default: T = Mat4.translation(0,0,0);
+                    break;
+            }
+        }
+        this.model_transform = (this.model_transform).times(T);
+            
+
+        this.shapes.pacman.draw(context, program_state, this.model_transform, this.materials.pacman);
+
+        let desired;
+        if (this.follow)
+            desired = Mat4.inverse((this.model_transform).times(Mat4.translation(0,3,5)).times(Mat4.rotation(-Math.PI/12,1,0,0)));
+        else
+            desired = Mat4.look_at(vec3(0, 50, 10), vec3(0, 0, -5), vec3(0, 0, -1));//Mat4.inverse((this.model_transform).times(Mat4.translation(0,50,10)).times(Mat4.rotation(-Math.PI/2,1,0,0)));
+            
+        desired = desired.map((x,i) => Vector.from(program_state.camera_inverse[i]).mix(x, 0.15));
+        program_state.set_camera(desired);
+    }
+
+    getAngle(dir){
+        let angle;
+        switch(dir){
+            case 'l': angle = Math.PI/2;
+                break;
+            case 'r': angle = -Math.PI/2;
+                break;
+            case 'b': angle = Math.PI;
+                break;
+            default: angle = 0;
+                break;
+        };
+        return angle;
+    }
+
+    getNewDir(){
+        switch(this.abs_dir){
+            case 'l':
+                switch(this.dir){
+                    case 'l': return 'b';
+                    case 'r': return 'f';
+                    case 'b': return 'r';
+                    default: break;
+                };
+            case 'r':
+                switch(this.dir){
+                    case 'l': return 'f';
+                    case 'r': return 'b';
+                    case 'b': return 'l';
+                    default: break;
+                };
+                break;
+            case 'b':
+                switch(this.dir){
+                    case 'l': return 'r';
+                    case 'r': return 'l';
+                    case 'b': return 'f';
+                    default: break;
+                };
+                break;
+            case 'f':
+                if (this.dir != null && this.dir !== 's')
+                    return this.dir;
+                break;
+            default:
+                break;
+        };
+        return this.abs_dir;
     }
 
     make_wall(context, program_state, loc, length, vert=false, width=1){
@@ -175,224 +308,5 @@ export class Demo3 extends Scene {
 
         this.make_side(context, program_state);
         this.make_side(context, program_state, true);
-    }
-
-    display(context, program_state) {
-        // display():  Called once per frame of animation.
-        
-        // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
-        if (!context.scratchpad.controls) {
-            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
-            // Define the global camera and projection matrices, which are stored in program_state.
-            program_state.set_camera(this.initial_camera_location);
-        }
-
-        program_state.projection_transform = Mat4.perspective(
-            Math.PI / 4, context.width / context.height, .1, 1000);
-
-        const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
-
-        const light_position = vec4(0, 10, 10, 1);
-
-        // The parameters of the Light are: position, color, size
-        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
-
-        this.make_walls(context, program_state);
-        
-        let model_transform = Mat4.identity().times(Mat4.translation(this.posX, 0, this.posZ));
-
-        switch(this.dir){
-            case 'f':
-                this.posZ -= 0.1;
-                break;
-            case 'l':
-                this.posX -= 0.1;
-                break;
-            case 'r':
-                this.posX += 0.1;
-                break;
-            case 'b':
-                this.posZ += 0.1;
-                break;
-            default:
-                break;
-        };
-
-        this.shapes.pacman.draw(context, program_state, model_transform, this.materials.pacman);
-        if (this.dir != null){
-            let desired = Mat4.inverse(model_transform.times(Mat4.translation(0,0,5)));
-            //program_state.set_camera(desired);
-        }
-    }
-}
-
-class Shader_Version_1 extends Shader {
-    constructor() {
-        super();
-    }
-
-    shared_glsl_code() {
-        // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
-        return ` 
-        precision mediump float;
-        `;
-    }
-
-    vertex_glsl_code() {
-        // ********* VERTEX SHADER *********
-        return this.shared_glsl_code() + `
-            attribute vec3 position, normal;       
-            
-            uniform mat4 model_transform;
-            uniform mat4 projection_camera_model_transform;
-    
-            void main(){                                                                   
-                // The vertex's final resting place (in NDCS):
-                gl_Position = projection_camera_model_transform * vec4( position, 1.0 ); // <---
-            } `;
-    }
-
-    fragment_glsl_code() {
-        // ********* FRAGMENT SHADER *********
-        return this.shared_glsl_code() + `
-            uniform vec4 shape_color; // <---
-        
-            void main(){                                                           
-                gl_FragColor = shape_color;
-            } `;
-    }
-
-    update_GPU(context, gpu_addresses, graphics_state, model_transform, material) {
-        // update_GPU():  Defining how to synchronize our JavaScript's variables to the GPU's:
-        const [P, C, M] = [graphics_state.projection_transform, graphics_state.camera_inverse, model_transform],
-            PCM = P.times(C).times(M);
-        context.uniformMatrix4fv(gpu_addresses.model_transform, false, Matrix.flatten_2D_to_1D(model_transform.transposed()));
-        context.uniformMatrix4fv(gpu_addresses.projection_camera_model_transform, false,
-            Matrix.flatten_2D_to_1D(PCM.transposed()));
-
-        // Set uniform parameters
-        context.uniform4fv(gpu_addresses.shape_color, material.color);
-    }
-}
-
-class Shader_Version_2 extends Shader {
-    constructor() {
-        super();
-    }
-
-    shared_glsl_code() {
-        // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
-        return ` 
-        precision mediump float;
-        
-        varying vec4 position_WCS; // <---
-        `;
-    }
-
-    vertex_glsl_code() {
-        // ********* VERTEX SHADER *********
-        return this.shared_glsl_code() + `
-            attribute vec3 position, normal;       
-            
-            uniform mat4 model_transform;
-            uniform mat4 projection_camera_model_transform;
-    
-            void main(){                                                                   
-                // The vertex's final resting place (in NDCS):
-                gl_Position = projection_camera_model_transform * vec4( position, 1.0 ); 
-                position_WCS = model_transform * vec4( position, 1.0 ); 
-            } `;
-    }
-
-    fragment_glsl_code() {
-        // ********* FRAGMENT SHADER *********
-        return this.shared_glsl_code() + `
-            uniform vec4 shape_color; 
-            uniform vec4 second_color; // <---
-        
-            void main(){              
-                float factor = 0.5 + 0.5 * sin(position_WCS.x * 5.0);
-                vec4 mixed_color = factor * second_color + (1.0 - factor) * shape_color;
-                gl_FragColor = mixed_color;
-            } `;
-    }
-
-    // CHANGED
-    send_material(gl, gpu, material) {
-        // send_material(): Send the desired shape-wide material qualities to the
-        // graphics card, where they will tweak the Phong lighting formula.
-        gl.uniform4fv(gpu.shape_color, material.color);
-        gl.uniform4fv(gpu.second_color, material.second_color); // <---
-        gl.uniform1f(gpu.ambient, material.ambient);
-        gl.uniform1f(gpu.diffusivity, material.diffusivity);
-        gl.uniform1f(gpu.specularity, material.specularity);
-        gl.uniform1f(gpu.smoothness, material.smoothness);
-    }
-
-    update_GPU(context, gpu_addresses, graphics_state, model_transform, material) {
-        // update_GPU():  Defining how to synchronize our JavaScript's variables to the GPU's:
-        const [P, C, M] = [graphics_state.projection_transform, graphics_state.camera_inverse, model_transform],
-            PCM = P.times(C).times(M);
-        context.uniformMatrix4fv(gpu_addresses.model_transform, false, Matrix.flatten_2D_to_1D(model_transform.transposed()));
-        context.uniformMatrix4fv(gpu_addresses.projection_camera_model_transform, false,
-            Matrix.flatten_2D_to_1D(PCM.transposed()));
-
-        // Set uniform parameters
-        context.uniform4fv(gpu_addresses.shape_color, material.color);
-        context.uniform4fv(gpu_addresses.second_color, material.second_color); // <---
-    }
-}
-
-class Shader_Version_3 extends Shader {
-    constructor() {
-        super();
-    }
-
-    shared_glsl_code() {
-        // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
-        return ` 
-        precision mediump float;
-        
-        varying vec4 position_OCS; // <---
-        `;
-    }
-
-    vertex_glsl_code() {
-        // ********* VERTEX SHADER *********
-        return this.shared_glsl_code() + `
-            attribute vec3 position, normal;       
-            
-            uniform mat4 model_transform;
-            uniform mat4 projection_camera_model_transform;
-    
-            void main(){                                                                   
-                // The vertex's final resting place (in NDCS):
-                gl_Position = projection_camera_model_transform * vec4( position, 1.0 ); 
-                position_OCS = vec4( position, 1.0 ); // <---
-            } `;
-    }
-
-    fragment_glsl_code() {
-        // ********* FRAGMENT SHADER *********
-        return this.shared_glsl_code() + `
-            uniform vec4 shape_color; 
-        
-            void main(){              
-                float factor = 0.5 + 0.5 * sin(position_OCS.x * 10.0 + position_OCS.y * 10.0);
-                vec4 mixed_color =  vec4(shape_color.xyz, factor);
-                gl_FragColor = mixed_color;
-            } `;
-    }
-
-    update_GPU(context, gpu_addresses, graphics_state, model_transform, material) {
-        // update_GPU():  Defining how to synchronize our JavaScript's variables to the GPU's:
-        const [P, C, M] = [graphics_state.projection_transform, graphics_state.camera_inverse, model_transform],
-            PCM = P.times(C).times(M);
-        context.uniformMatrix4fv(gpu_addresses.model_transform, false, Matrix.flatten_2D_to_1D(model_transform.transposed()));
-        context.uniformMatrix4fv(gpu_addresses.projection_camera_model_transform, false,
-            Matrix.flatten_2D_to_1D(PCM.transposed()));
-
-        // Set uniform parameters
-        context.uniform4fv(gpu_addresses.shape_color, material.color);
     }
 }
